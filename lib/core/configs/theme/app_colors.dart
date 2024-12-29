055642f94c3a6f18a32f2f890b180ea1c9226d78
nnddev <nnddev.official@gmail.com>
2024-12-30 02:07:52 +0700

    Update SVG Image

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primary = Color(0xff8E6CEF);
   static const background = Color(0xff1D182A);
-  static const secondBackGround = Color(0xff342F3F);
+  static const secondBackground = Color(0xff342F3F);
 }
---
+++ lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce/core/configs/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: SvgPicture.asset(assetName),
+      ),
+    );
+  }
+}
